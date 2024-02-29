959c02496b82d0f1b2befd826a43ac8f02e829ad
kimgr1221 <kimgyulee00@gmail.com>
2024-02-29 22:15:36 +0900

    frontend-react

--- src/components/Mainpage/Header.js
+++ src/components/Mainpage/Header.js
@@ -1,18 +1,24 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import './Header.css';
 import { useNavigate } from 'react-router-dom';
-import { BiUserCircle} from 'react-icons/bi';
+import { BiUserCircle } from 'react-icons/bi';
 
 const Header = () => {
     const navigate = useNavigate();
+    const [isLoggedIn, setIsLoggedIn] = useState(false);
+
+    useEffect(() => {
+        const token = localStorage.getItem('token');
+        setIsLoggedIn(!!token);
+    }, []);
 
     const handleLoginButtonClick = () => {
         navigate('login');
-    }
+    };
 
     const handleUserIconClick = () => {
         navigate('mypage');
-    }
+    };
 
     const handleScrollToSection = (id) => {
         const section = document.getElementById(id);
@@ -28,21 +34,31 @@ const Header = () => {
                     <img src='/images/logo.png' alt="푸드픽스 로고" />
                 </div>
 
-                <button className="nav-button" onClick={() => handleScrollToSection('main')}>푸드픽스 소개</button>
-                <button className="nav-button" onClick={() => handleScrollToSection('info')}>정보 등록 및 수정</button>
-                <button className="nav-button" onClick={() => handleScrollToSection('res')}>예약 관리</button>
-                <button className="nav-button" onClick={() => handleScrollToSection('order')}>주문 관리</button>
+                <div className="nav-buttons">
+                    <button className="nav-button" onClick={() => handleScrollToSection('main')}>푸드픽스 소개</button>
+                    <button className="nav-button" onClick={() => handleScrollToSection('info')}>정보 등록 및 수정</button>
+                    <button className="nav-button" onClick={() => handleScrollToSection('res')}>예약 관리</button>
+                    <button className="nav-button" onClick={() => handleScrollToSection('order')}>주문 관리</button>
+                </div>
 
                 <div className="user">
+                    {!isLoggedIn && (
                     <button className="button" onClick={handleLoginButtonClick}>로그인</button>
+                )}
+                
+                {isLoggedIn && (
                     <BiUserCircle className="user-icon" onClick={handleUserIconClick} size={40} />
+                )}
                 </div>
+                
             </div>
         </div>
     );
 };
 
 export default Header;
+
+
 
 
 
--- src/components/Mypage/Side.js
+++ src/components/Mypage/Side.js
@@ -9,14 +9,47 @@ const Side = ({ onEditClick, onLeaveClick }) => {
         navigate(-1);
     }
 
+    const handleLogout = async () => {
+        const token = localStorage.getItem('token'); 
+        console.log('Token:', token);
+    
+        try {
+            const response = await fetch('/admin/logout', {
+                method: 'POST',
+                headers: {
+                    'Authorization': `Bearer ${token}`,
+                    'Content-Type': 'application/json',
+                },
+                credentials: 'include',
+            });
+    
+            if (response.ok) {
+                console.log('Logout successful');
+                localStorage.removeItem('token');
+                
+                alert('로그아웃 되었습니다.');
+                navigate('/');
+            } else {
+                console.error('Logout failed');
+                alert('로그아웃 실패');
+            }
+        } catch (error) {
+            console.error('Error during logout:', error.message);
+        }
+    };
+    
+
     return (
         <div className="side">
             <img src='/images/usericon.png' alt='usericon' className="profile" />
             <div className="edit-user" onClick={onEditClick}>회원 정보 수정</div> 
-            <div className="leave-user" onClick={onLeaveClick}>회원 탈퇴</div> 
+            <div className="leave-user" onClick={onLeaveClick}>회원 탈퇴</div>
+            <div className="logout" onClick={handleLogout}>로그아웃</div> 
             <div onClick={goBack} className="back">뒤로 가기</div>
         </div>
     );
 };
 
 export default Side;
+
+
